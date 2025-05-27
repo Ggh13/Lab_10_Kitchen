7280d8fb9d0d08331128662cbd099b98dffe8efa
GermanPikel <germanpikel@gmail.com>
2025-05-27 22:02:05 +0300

    added Salad class

---
+++ RestaurantMenu/Model/Core/MealDir/Salad.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Core.MealDir
+{
+    public class Salad : Meal
+    {
+        public Salad(string name, int price, string description) : base(name, price, description) { }
+    }
+}
